105041ca7587919557de84af4c10b1d956affc38
jinxin26 <xin.jin@thoughtworks.com>
2021-12-14 15:39:28 +0800

    add card-buttons

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,9 +1,24 @@
 console.log('Hello World!');
 
+const onBtnClick = function(t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize(
   {
     'card-badges': function(t, opts) {
       return [];
+    },
+    'card-buttons': function(t, opts) {
+      return [{
+        text: 'Demand-Power-Up',
+        callback: onBtnClick,
+        condition: 'edit'
+      }, {
+        text: 'Open',
+        condition: 'always',
+        target: 'Trello Developer Site'
+      }];
     }
   }
 );
