3a22d6a9cbf63ed21ea1830e915a56cd3ea15d9e
jinxin26 <xin.jin@thoughtworks.com>
2021-12-16 00:19:18 +0800

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -10,8 +10,7 @@ const onSaveBtnClicked = function (t, opts) {
 const onSetBtnCLicked = function(t) {
   console.log('before set', changeCount);
   changeCount = changeCount + 1;
-  t.card('name')
-    .set('card', 'shared', {changeCount}).then(res => {
+    t.set('card', 'shared', {changeCount: changeCount}).then(res => {
     console.log(res);
     return [{
       dynamic: function() {
