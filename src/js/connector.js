663ca702194ce8294c5796f0dc4c2d3d5128ba8b
jinxin26 <xin.jin@thoughtworks.com>
2021-12-16 11:25:11 +0800

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -74,7 +74,7 @@ window.TrelloPowerUp.initialize(
     },
     'card-detail-badges': function (t, opts) {
       return t.card('name')
-        .get('all').then(res => console.log('this is all', res))
+        .then(res => console.log('this is all', res))
         // // .get('name')
         // // .then(
         // //   function(cardName) {
