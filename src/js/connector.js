e1c74ced17f72fc3bc60e3cebe5df762aaf85fa9
jinxin26 <xin.jin@thoughtworks.com>
2021-12-16 19:48:16 +0800

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -38,46 +38,18 @@ window.TrelloPowerUp.initialize(
       }];
     },
     'card-badges': function(t, opts) {
-      t.get();
-      let cardAttachments = t.card('attachments');
-      // let cardAttachments = opts.attachments;
-      return t.card("name")
-        .get("name")
-        .then(function(cardName) {
-        console.log('card name  ' + cardName);
-        return [
-          {
-          dynamic: function() {
-            return {
-              text: "Dynamic" + (Math.random() * 100).toFixed(0).toString(),
-              color: "green",
-              refresh: 10,
-            };
-          },
-        },
-          {
-            text: "Static",
-            color: null,
-          }];
-      });
+      const cardId = t.getContext().card;
+      return t.get(cardId, 'shared', 'demandChangeCount').then(res => {
+        if (res) {
+          return [{
+            color: 'red'
+          }]
+        }
+      })
     },
     'card-buttons': cardButtons,
     'card-detail-badges': function (t, opts) {
       const cardId = t.getContext().card;
-      return t.get('board', 'shared', 'demandChangeCount').then(res => {
-        demandChangeCount = res ? res : 0;
-        console.log('demandChangeCount', demandChangeCount);
-        return [{
-          dynamic: function () {
-            return {
-              title: 'Changes',
-              text: demandChangeCount,
-              color: 'red',
-              refresh: 10
-            };
-          },
-        }];
-      });
       return t.get(cardId, 'shared', 'demandChangeCount').then(res => {
         demandChangeCount = res ? res : 0;
         console.log('demandChangeCount', demandChangeCount);
