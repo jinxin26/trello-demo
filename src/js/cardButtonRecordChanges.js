bc18d3a7256242f0f5184a34ac191d490e78d6b2
jinxin26 <xin.jin@thoughtworks.com>
2021-12-20 19:45:55 +0800

    test: add demandInfo

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -7,10 +7,22 @@ t.get(context.card, 'shared', 'demandChangeCount').then(demandChangeCountInRespo
     showDemandChangeCount(`total changes: ${demandChangeCount}`);
 });
 
-t.remove(context.card, 'shared', 'demandInfo').then(res => console.log("this is remove", res));
+// use remove to delete a certain info
+// t.remove(context.card, 'shared', 'demandInfo').then(res => console.log("this is remove", res));
 
-t.get(context.card, 'shared', 'demandInfo').then(res => console.log("can i get res?", res));
-// let demandInfo = [];
+let demandInfo = [];
+t.card("desc").then(curDesc => {
+    console.log('this is curDesc', curDesc);
+    t.get(context.card, 'shared', 'demandInfo').then(totalDesc => {
+        console.log('this is totalDesc', totalDesc);
+        if (!totalDesc) {
+            demandInfo.push(curDesc);
+            console.log('this is demandInfo', demandInfo);
+            t.set(context.card, 'shared', {demandInfo});
+            t.get(context.card, 'shared', 'demandInfo').then(res => console.log('this is demandInfo after set', res));
+        }
+    })
+})
 // t.get(context.card, 'shared', 'demandInfo').then(res => {
 //     if(res.length === 0) {
 //         t.card("desc").then(cards => {
@@ -38,12 +50,19 @@ onSaveBtnClick = () => {
     t.set(context.card, 'shared', {demandChangeCount});
     console.log("demandChangeCount is saved!");
     showDemandChangeCount(`total changes: ${demandChangeCount} (save successfully!)`);
-    t.card('desc').then(res => {
-        console.log('init res', res);
-        demandInfo.push(res);
-        console.log('changed demandInfo', demandInfo);
-    });
-    t.set(context.card, 'shared', {demandInfo}).then(res => console.log('current demandInfo', res));
+
+    t.card("desc").then(curDesc => {
+        console.log('this is curDesc after save', curDesc);
+        t.get(context.card, 'shared', 'demandInfo').then(totalDesc => {
+            console.log('this is totalDesc after save', totalDesc);
+            if(curDesc !== totalDesc[totalDesc.length - 1]) {
+                demandInfo.push(curDesc);
+                console.log('this is demandInfo after save', demandInfo);
+                t.set(context.card, 'shared', {demandInfo})
+                t.get(context.card, 'shared', 'demandInfo').then(res => console.log('this is demandInfo after set and save', res));
+            }
+        })
+    })
 }
 
 showDemandChangeCount = demandChangeCount => {
