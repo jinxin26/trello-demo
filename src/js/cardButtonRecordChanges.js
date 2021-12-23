cd649d55700483d9453c7923e38a74a0ce0b3d52
jinxin26 <xin.jin@thoughtworks.com>
2021-12-23 14:48:06 +0800

    test: next page

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -28,7 +28,7 @@ const addBtnForVersionRecord = (list, versionRecord, curPage) => {
         versionRecord.appendChild(button);
     }
 
-    if(list.data.length > 5 && curPage !== 0) {
+    if(list.data.length > 5 || curPage !== 0) {
         //add btn for changing page
         const prevPage = document.createElement("button");
         prevPage.textContent = "<";
