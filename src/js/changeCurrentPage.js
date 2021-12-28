e3f21ba95fb652d7bd0afb522733ef570c7bc9d9
jinxin26 <xin.jin@thoughtworks.com>
2021-12-28 15:58:48 +0800

    test: try to add tab by click button

--- src/js/changeCurrentPage.js
+++ src/js/changeCurrentPage.js
@@ -1,9 +1,10 @@
 const t = window.TrelloPowerUp.iframe();
 
 window.showStatistics = function showStatistics() {
-  return t.modal({
-    title: 'Requirement Change Analysis',
-    url: './requirementChangeAnalysis.html',
-    fullscreen: true
-  });
+  // return t.modal({
+  //   title: 'Requirement Change Analysis',
+  //   url: './requirementChangeAnalysis.html',
+  //   fullscreen: true
+  // });
+  console.log('this is showStatistics');
 };
