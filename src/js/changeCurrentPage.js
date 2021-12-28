154c07670ca8ce7c8e7fe6134a2ebcb71094cf94
jinxin26 <xin.jin@thoughtworks.com>
2021-12-28 16:02:34 +0800

    test: try to add tab by click button(add script in changeCurrentPage.html)

--- src/js/changeCurrentPage.js
+++ src/js/changeCurrentPage.js
@@ -1,6 +1,6 @@
 const t = window.TrelloPowerUp.iframe();
 
-window.showStatistics = function showStatistics() {
+showStatistics = () => {
   // return t.modal({
   //   title: 'Requirement Change Analysis',
   //   url: './requirementChangeAnalysis.html',
