b939ed0117372570e6a8cbdfa4b3aab5b94ec390
jinxin26 <xin.jin@thoughtworks.com>
2021-12-29 16:02:44 +0800

    test: show two different pages using button

--- src/js/changeCurrentPage.js
+++ src/js/changeCurrentPage.js
@@ -3,13 +3,15 @@ const t = window.TrelloPowerUp.iframe();
 showStatistics = () => {
   return t.modal({
     title: 'Requirement Change Analysis',
-    url: './requirementChangeAnalysis.html'
+    url: './requirementChangeAnalysis.html',
+    fullscreen: true
   });
 };
 
 showVersionRecord = () => {
   return t.modal({
     title: 'Version Record',
-    url: './requirementChangeAnalysis.html'
+    url: './requirementChangeAnalysis.html',
+    fullscreen: true
   })
 }
