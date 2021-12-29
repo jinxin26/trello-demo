7476fa72e981a7721643b64f9e95470824daf587
jinxin26 <xin.jin@thoughtworks.com>
2021-12-29 14:23:20 +0800

    fix: number of version button

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -22,6 +22,9 @@ const addBtnForVersionRecord = (list, versionRecord, curPage) => {
     document.body.appendChild(versionRecord);
     versionRecord.id = "versionRecord";
 
+    console.log('this is list', list.data);
+    list.data.filter(item => item.descriptions !== 'v0.0');
+    console.log('this is list after filter', list.data);
     for (let i = list.data.length - 1 - curPage * 5; i >= list.data.length - curPage * 5 - 5 && i >= 0; i--) {
         if(list.data[i].version !== 'v0.0') {
             const button = document.createElement("button");
