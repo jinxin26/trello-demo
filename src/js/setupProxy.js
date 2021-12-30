221a34d4b640834bfb77f685d0fc87fa17d18218
jinxin26 <xin.jin@thoughtworks.com>
2021-12-30 23:17:50 +0800

    test: connect to server

--- src/js/setupProxy.js
+++ src/js/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        'http://122.51.213.254:8086/',
+        'http://localhost:8086/',
         createProxyMiddleware(
             {
                         target: 'http://122.51.213.254:8086/',
