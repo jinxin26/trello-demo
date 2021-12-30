c014302154ee6f5b12cb9d700b8faa50767a6350
jinxin26 <xin.jin@thoughtworks.com>
2021-12-30 23:45:44 +0800

    test: connect to server using 122.51.213.254 and delete localhost

--- src/js/setupProxy.js
+++ src/js/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        'http://localhost:8086/',
+        'http://122.51.213.254:8086/',
         createProxyMiddleware(
             {
                         target: 'http://122.51.213.254:8086/',
