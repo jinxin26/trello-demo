6e32fa851dac3dcec65b8a87b7e12ade5a02b06f
jinxin26 <xin.jin@thoughtworks.com>
2021-12-31 10:08:34 +0800

    test: use http://localhost:8086/

--- src/js/setupProxy.js
+++ src/js/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        'https://friendly-sinoussi-3fd1f5.netlify.app/',
+        'http://localhost:8086/',
         createProxyMiddleware(
             {
                         target: 'http://122.51.213.254:8086/',
