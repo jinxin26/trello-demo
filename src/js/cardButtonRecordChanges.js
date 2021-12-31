1fa983ab38a102ef123ee767b1662f4c73916806
jinxin26 <xin.jin@thoughtworks.com>
2021-12-31 09:54:40 +0800

    test: use /description

--- src/js/cardButtonRecordChanges.js
+++ src/js/cardButtonRecordChanges.js
@@ -64,7 +64,7 @@ const addBtnForVersionRecord = (list, versionRecord, curPage) => {
 
 function onVersionBtnCLick(text) {
     console.log('text', text);
-    axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+    axios.get(`/description/${context.card}`).then(list => {
         console.log('length of list', list.data.length);
 
         const versionNum = parseInt(text.substring(1));
@@ -102,7 +102,7 @@ function onVersionBtnCLick(text) {
 }
 
 const getVersionRecord = () => {
-    axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+    axios.get(`/description/${context.card}`).then(list => {
         const versionRecord = document.getElementById("versionRecord");
         let curPage = 0;
         addBtnForVersionRecord(list, versionRecord, curPage);
@@ -144,10 +144,10 @@ window.onSaveBtnClick = function onSaveBtnClick () {
         info.cardId = res.id;
         info.descriptions = res.desc;
         info.version = `v${demandChangeCount}.0`;
-        axios.post("http://localhost:8086/description", info).then(res => {
+        axios.post("/description", info).then(res => {
             console.log('this is post info', res);
 
-            axios.get(`http://localhost:8086/description/${context.card}`).then(list => {
+            axios.get(`/description/${context.card}`).then(list => {
                 let versionRecord = document.getElementById("versionRecord");
                 addBtnForVersionRecord(list, versionRecord, 0);
             });
